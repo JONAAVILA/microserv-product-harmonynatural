3d00238710e269f009ca3a68969c133e19f3aee6
JONAAVILA <jona_1969@hotmail.com>
2024-12-05 13:05:34 -0300

    mercado pago config fixed

--- src/payments/mercadoPago/createPreference.js
+++ src/payments/mercadoPago/createPreference.js
@@ -2,7 +2,7 @@ import { MercadoPagoConfig } from 'mercadopago'
 
 const { ACCESS_TOKEN_MP } = process.env
 
-const client = new MercadoPagoConfig.create(
+const client = new MercadoPagoConfig(
     {
         accessToken: ACCESS_TOKEN_MP
     }
