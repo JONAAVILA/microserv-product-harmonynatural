1b041d2825c075cb300972295e7ade3135d57549
JONAAVILA <jona_1969@hotmail.com>
2024-12-06 10:07:04 -0300

    handler delete fixed

--- src/handlers/delete/handlerDelete.js
+++ src/handlers/delete/handlerDelete.js
@@ -3,11 +3,15 @@ import { models } from "../../db.js"
 const { Product } = models
 
 const handlerDelete = async (id)=>{
-    const product = await Product.findByPk(id)
-    if(!product) throw new Error('No se encontro el producto');
-
-    product.active = false
-    await product.save()
+    const product = await Product.update(
+        {
+            active:false
+        },
+        {
+            where:{id}
+        }
+    )
+    if(!product) throw new Error('No se encontro el producto')
 
     return 'Producto eliminado'
 }
